dd0165350513667fdf5a04f9dbd5e12e855b4143
dantheman8300 <daniel.7039@hotmail.com>
2023-10-23 20:20:26 -0700

    main window title

--- move-studio-app/app/build/mainWindow.tsx
+++ move-studio-app/app/build/mainWindow.tsx
@@ -3,25 +3,24 @@ import { Cross2Icon } from "@radix-ui/react-icons";
 import CodeEditor from "./codeEditor";
 import PackageWindow from "./PackageWindow";
 import { useEffect } from "react";
+import { CubeSpinner, MagicSpinner, WaveSpinner } from "react-spinners-kit";
+import { TypeAnimation } from 'react-type-animation';
 
 
-const mockTabs = [
-  {
-    type: 'code', 
-    path: 'demoPackage/sources/party.move', 
-    name: 'party.move'
-  },
-  {
-    type: 'code', 
-    path: 'demoPackage/move.toml', 
-    name: 'move.toml'
-  }, 
-  {
-    type: 'package', 
-    digestId: '0x02',
-    name: 'demoPackage'
-  }
-] as ({type: 'code', path: string, name: string} | {type: 'package', digestId: string, name: string})[]
+const quotes = [
+  "First, solve the problem. Then, write the code.\n - John Johnson", 
+  "I’m not a great programmer; I’m just a good programmer with great habits.\n - Kent Beck",
+  "It’s not a bug; it’s an undocumented feature.\n - Anonymous",
+  "Talk is cheap. Show me the code.\n - Linus Torvalds",
+  "Code is like humor. When you have to explain it, it’s bad.\n - Cory House",
+  "Fix the cause, not the symptom.\n - Steve Maguire",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.\n - Martin Fowler",
+  "You might not think that programmers are artists, but programming is an extremely creative profession. It’s logic-based creativity.\n - John Romero", 
+  "Programming is learned by writing programs.\n - Brian Kernighan",
+  "If, at first, you do not succeed, call it version 1.0.\n - Khayri R.R. Woulfe"
+]
+
+const interleave = (arr: any, thing: any) => [].concat(...arr.map((n: any) => [n, thing])).slice(0, -1)
 
 
 export default function MainWindow(
@@ -89,6 +88,33 @@ export default function MainWindow(
             )
           }
         })
+      }
+      {
+        tabs.length == 0 &&
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          <MagicSpinner color="#fff" size={60} />
+          <span className="font-mono text-white text-xl pt-4 antialiased">
+            Move Studio IDE
+          </span>
+          <span className="font-mono text-teal-600 w-[300px] text-center whitespace-pre-line break-words">
+            <TypeAnimation
+              sequence={[
+                ...interleave(
+                  quotes.sort(() => 0.5 - Math.random()), 
+                  2000
+                ),
+                "That's all folks!", 
+                5000, 
+                "Seriously, that's all folks!",
+                5000,
+                "Ok, I'm done now."
+              ]}
+              wrapper="span"
+              cursor={true}
+              speed={25}
+            />
+          </span>
+        </div>
       }
       </div>
     </Tabs>
