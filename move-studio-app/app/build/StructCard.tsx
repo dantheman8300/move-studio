ab5473b6f893ef29bfcfe1bbe69036e9e4e5f86c
dantheman8300 <daniel.7039@hotmail.com>
2023-10-30 06:49:24 -0700

    Finished cases for structs

--- move-studio-app/app/build/StructCard.tsx
+++ move-studio-app/app/build/StructCard.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 
 export default function StructCard(
@@ -17,9 +18,13 @@ export default function StructCard(
                 <div key={index}>
                   <span className="font-mono text-teal-800">{parameter.isPhantom && 'phantom'} T{index}</span>
                   {
-                    parameter.constraints.abilities.map((ability: string) => {
-                      return <span key={index} className="text-teal-800">
-                        {ability}
+                    parameter.constraints.abilities.length > 0 &&
+                    <span className="text-slate-400"> has </span>
+                  }
+                  {
+                    parameter.constraints.abilities.map((ability: string, index: number) => {
+                      return <span key={index} className="text-teal-800 font-mono">
+                        {`${ability}${index < parameter.constraints.abilities.length - 1 ? ', ' : ''}`}
                       </span>
                     })
                   }
@@ -34,13 +39,52 @@ export default function StructCard(
         <div className="ps-2 flex flex-col items-start justify-start w-full">
           {
             props.data.fields.map((field: any, index: number) => {
-
+              console.log('field', field)
               if (field.type.Struct != undefined) {
                 return (
                   <div key={index}>
                     <span className="font-mono text-slate-400">{field.name} - </span><span className="font-mono text-teal-800 hover:text-teal-500">{field.type.Struct.address}::{field.type.Struct.module}::{field.type.Struct.name}</span>
                   </div>
                 )
+              } else if (field.type.TypeParameter != undefined) {
+                return (
+                  <div key={index}>
+                    <span className="font-mono text-slate-400">{field.name} - </span><span className="font-mono text-teal-800 hover:text-teal-500">T{field.type.TypeParameter}</span>
+                  </div>
+                )
+              
+              } else if (field.type.Vector != undefined) {
+                return (
+                  <div key={index}>
+                    <span className="font-mono text-slate-400">{field.name} - </span><span className="font-mono text-teal-800 hover:text-teal-500">
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>[{
+                            field.type.Vector.Struct != undefined ?
+                              `${field.type.Vector.Struct.address}::${field.type.Vector.Struct.module}::${field.type.Vector.Struct.name}`.length < 10 ?
+                                `${field.type.Vector.Struct.address}::${field.type.Vector.Struct.module}::${field.type.Vector.Struct.name}`
+                              :
+                                `${field.type.Vector.Struct.address}::${field.type.Vector.Struct.module}::${field.type.Vector.Struct.name}`.slice(0, 4) + '...' + `${field.type.Vector.Struct.address}::${field.type.Vector.Struct.module}::${field.type.Vector.Struct.name}`.slice(-4)
+                            :
+                              `${field.type.Vector.slice(0, 4)}...${field.type.Vector}`.length < 10 ?
+                                `${field.type.Vector.slice(0, 4)}...${field.type.Vector}`
+                              :
+                                `${field.type.Vector.slice(0, 4)}...${field.type.Vector.slice(-4)}`
+                          }]</TooltipTrigger>
+                          <TooltipContent>
+                            <p>[{
+                            field.type.Vector.Struct != undefined ?
+                              `${field.type.Vector.Struct.address}::${field.type.Vector.Struct.module}::${field.type.Vector.Struct.name}`
+                            :
+                              `${field.type.Vector.slice(0, 4)}...${field.type.Vector}`
+                          }]</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </span>
+                  </div>
+                )
+
               } else {
                 return (
                   <div key={index}>
