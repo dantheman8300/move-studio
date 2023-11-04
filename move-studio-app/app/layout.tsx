cad44bc3ea20ed48969f3834c87c9e9ddd4eefd5
dantheman8300 <daniel.7039@hotmail.com>
2023-11-03 20:17:06 -0700

    installed analytics

--- move-studio-app/app/layout.tsx
+++ move-studio-app/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 
 import { WalletProvider } from "@suiet/wallet-kit";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/react';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,7 +21,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <WalletProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {children}  
+              {children}
+              <Analytics />
               <Toaster />
           </ThemeProvider>
         </WalletProvider>
