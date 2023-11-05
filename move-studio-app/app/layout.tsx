4f4d07a7d39aa56a3c87f46a779a43c4e0a30164
dantheman8300 <daniel.7039@hotmail.com>
2023-11-05 12:09:41 -0800

    remove light mode - hopefully

--- move-studio-app/app/layout.tsx
+++ move-studio-app/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <WalletProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="dark">
               {children}
               <Analytics />
               <Toaster />
