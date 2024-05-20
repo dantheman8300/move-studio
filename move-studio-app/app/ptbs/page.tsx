9b79809a5bb0941346abad5956a0f152fc454235
Daniel Leavitt <daniel.7039@hotmail.com>
2024-05-20 11:57:57 -0700

    page init

---
+++ move-studio-app/app/ptbs/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <div className="h-screen w-full max-w-screen flex flex-col items-center dark:bg-slate-950 overflow-hidden">
+      ptbs
+    </div>
+  )
+}
