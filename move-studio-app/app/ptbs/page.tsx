1228eaf30d33eebd28564197a65d617d837fbd91
Daniel Leavitt <daniel.7039@hotmail.com>
2024-05-20 12:04:18 -0700

    basic flow

--- move-studio-app/app/ptbs/page.tsx
+++ move-studio-app/app/ptbs/page.tsx
@@ -1,9 +1,10 @@
+import Flow from "./flow";
 
 
 export default function Page() {
   return (
-    <div className="h-screen w-full max-w-screen flex flex-col items-center dark:bg-slate-950 overflow-hidden">
-      ptbs
+    <div className="h-screen w-full max-w-screen flex flex-col items-center dark:bg-slate-950 overflow-hidden border">
+      <Flow />
     </div>
   )
 }
