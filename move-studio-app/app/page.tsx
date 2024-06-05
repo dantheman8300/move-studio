8fa3e13c4bc891d1ca34f94f0b92c308638a333a
pedrocruzio <pedro@rokailabs.com>
2024-06-04 20:20:03 -0400

    fix: responsive landing page

--- move-studio-app/app/page.tsx
+++ move-studio-app/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   const [activeTab, setActiveTab] = useState('editor')
 
   return (
-    <div className='w-screen min-h-screen h-full bg-slate-950 '>
+    <div className='w-screen min-h-screen h-full bg-slate-950'>
       <div className='fixed w-full backdrop-filter backdrop-blur-sm bg-opacity-30 border-b border-slate-500'>
         <div className="flex w-full flex-row justify-between items-center my-2 px-3 h-[50px] gap-2">
           <TypographyH2>Move Studio</TypographyH2>
@@ -32,29 +32,29 @@ export default function Home() {
         </div>
         {/* <Separator /> */}
       </div>
-      <div className='w-full min-h-[500px] flex flex-col items-center justify-start pt-[100px]'>
-        <div className='flex flex-col items-center justify-start'>
-          <span className='text-5xl font-bold text-slate-100'>The best <span className='bg-gradient-to-r from-sky-300 to-indigo-400 text-transparent bg-clip-text'>Sui</span> Development Suite</span>
-          <span className='text-xl text-slate-400'>
+      <div className='w-full min-h-[500px] flex flex-col items-center justify-start pt-[100px] px-4 md:px-0'>
+        <div className='flex flex-col items-center justify-start text-center'>
+          <span className='text-3xl md:text-5xl font-bold text-slate-100'>The best <span className='bg-gradient-to-r from-sky-300 to-indigo-400 text-transparent bg-clip-text'>Sui</span> Development Suite</span>
+          <span className='text-lg md:text-xl text-slate-400'>
             A complete Sui development suite built into your browser.
           </span>
         </div>
         {/* <Image src={CodeEditorImage} alt='' width={200} height={200} /> */}
         <a href='/build'>
-          <Button className='animate-pulse hover:animate-none h-12 px-4 text-lg mt-10 shadow shadow-[3px_3px_20px_-5px_rgb(16,185,129,0.8),0px_-5px_20px_-5px_rgb(99,102,241,0.5),0_5px_20px_-5px_rgb(245,158,11,0.5)]' onClick={() =>{track('start_building_button')}}>
+          <Button className='animate-pulse hover:animate-none h-12 px-4 text-lg mt-10 shadow shadow-[3px_3px_20px_-5px_rgb(16,185,129,0.8),0px_-5px_20px_-5px_rgb(99,102,241,0.5),0_5px_20px_-5px_rgb(245,158,11,0.5)]' onClick={() => { track('start_building_button') }}>
             Start building
           </Button>
         </a>
         <div className={
-          'w-[1000px] min-h-[600px] mt-[75px] shadow-[0_0_75px_-10px_rgb(0,0,0,0.3)] hover:shadow-[0_0_100px_-25px_rgb(0,0,0,0.3)] transition-shadow rounded-xl bg-transparent border overflow-hidden' +
+          'w-full md:w-[1000px] min-h-[600px] mt-[75px] shadow-[0_0_75px_-10px_rgb(0,0,0,0.3)] hover:shadow-[0_0_100px_-25px_rgb(0,0,0,0.3)] transition-shadow rounded-xl bg-transparent border overflow-hidden' +
           `${activeTab === 'editor' ? ' shadow-amber-500/50 hover:shadow-amber-500 border-amber-500' : ''}` +
           `${activeTab === 'manager' ? ' shadow-emerald-500/50 hover:shadow-emerald-500 border-emerald-500' : ''}` +
           `${activeTab === 'explorer' ? ' shadow-indigo-500/50 hover:shadow-indigo-500 border-indigo-500' : ''}`
         }>
-          <div className="w-full flex flex-row items-start justify-around border-b">
-            <div 
+          <div className="w-full flex flex-row items-start justify-around border-b text-xs md:text-lg">
+            <div
               className={
-                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer' + 
+                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer ' +
                 `${activeTab === 'editor' ? ' bg-gradient-to-b from-slate-950 to-amber-500/20' : ''}`
               }
               onClick={() => {
@@ -73,9 +73,9 @@ export default function Home() {
               </span>
             </div>
             <Separator orientation='vertical' className='h-[70px]' />
-            <div 
+            <div
               className={
-                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer' + 
+                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer' +
                 `${activeTab === 'manager' ? ' bg-gradient-to-b from-slate-950 to-emerald-500/20' : ''}`
               }
               onClick={() => {
@@ -94,9 +94,9 @@ export default function Home() {
               </span>
             </div>
             <Separator orientation='vertical' className='h-[70px]' />
-            <div 
+            <div
               className={
-                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer' + 
+                'h-[70px] grow flex flex-col items-center justify-center hover:cursor-pointer' +
                 `${activeTab === 'explorer' ? ' bg-gradient-to-b from-slate-950 to-indigo-500/20' : ''}`
               }
               onClick={() => {
@@ -117,13 +117,13 @@ export default function Home() {
           </div>
           {
             activeTab === 'editor' &&
-            <div className='h-[500px] flex flex-row items-center justify-around'>
-              <div className='w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
+            <div className='h-[500px] flex flex-col md:flex-row items-center justify-around p-8 md:p-2'>
+              <div className='w-full md:w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
                 <span className='text-3xl font-semibold bg-gradient-to-r from-yellow-300 to-amber-500 text-transparent bg-clip-text'>Code Editor</span>
                 <span className='text-slate-400'>Tailored to the Sui Move language</span>
                 <ul className='mt-2 list-disc text-slate-200'>
                   <li>Built in compiling</li>
-                  <li>Syntax highligting for the Sui Move language</li>
+                  <li>Syntax highlighting for the Sui Move language</li>
                   <li>Fully integrated with Move testing framework</li>
                 </ul>
               </div>
@@ -135,8 +135,8 @@ export default function Home() {
           }
           {
             activeTab === 'manager' &&
-            <div className='h-[500px] flex flex-row items-center justify-around'>
-              <div className='w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
+            <div className='h-[500px] flex flex-col md:flex-row items-center justify-around p-8 md:p-2'>
+              <div className='w-full md:w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
                 <span className='text-3xl font-semibold bg-gradient-to-r from-teal-300 to-emerald-500 text-transparent bg-clip-text'>Package manager</span>
                 <span className='text-slate-400'>Interact with deployed packages instantly</span>
                 <ul className='mt-2 list-disc text-slate-200'>
@@ -150,8 +150,8 @@ export default function Home() {
           }
           {
             activeTab === 'explorer' &&
-            <div className='h-[500px] flex flex-row items-center justify-around'>
-              <div className='w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
+            <div className='h-[500px] flex flex-col md:flex-row items-center justify-around p-8 md:p-2'>
+              <div className='w-full md:w-fit h-[450px] flex flex-col items-start justify-start ps-4'>
                 <span className='text-3xl font-semibold bg-gradient-to-r from-purple-300 to-indigo-500 text-transparent bg-clip-text'>Object explorer</span>
                 <span className='text-slate-400'>Access all on-chain objects</span>
                 <ul className='mt-2 list-disc text-slate-200'>
@@ -170,39 +170,39 @@ export default function Home() {
       </div>
       {/* <Separator /> */}
       <div className='mt-[100px] w-full h-fit flex flex-col items-center justify-start gap-10'>
-        <span className='mt-10 text-4xl font-bold text-slate-100'>Built for <span className='bg-gradient-to-r from-teal-300 to-emerald-500 text-transparent bg-clip-text'>Effiency</span></span>
-        <div className='w-[600px] h-[100px] border rounded-xl flex flex-row bg-gradient-to-b from-slate-950 to-emerald-500/20 shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-emerald-500/50'>
-          <div className='w-[200px] flex flex-col items-center justify-center'>
-            <span className='text-2xl font-bold flex flex-row items-center gap-2'><PackagePlus strokeWidth={1.25} className='h-5 w-5'/>1000+</span>
-            <span className='text-slate-400'>Deployed packages</span>
+        <span className='mt-10 text-4xl font-bold text-slate-100 text-center'>Built for <span className='bg-gradient-to-r from-teal-300 to-emerald-500 text-transparent bg-clip-text'>Efficiency</span></span>
+        <div className='w-full md:w-[600px] h-[100px] border rounded-xl flex flex-row bg-gradient-to-b from-slate-950 to-emerald-500/20 shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-emerald-500/50'>
+          <div className='w-full md:w-[200px] flex flex-col items-center justify-center'>
+            <span className='text-xs font-bold flex flex-row items-center gap-2'><PackagePlus strokeWidth={1.25} className='h-5 w-5' />1000+</span>
+            <span className='text-xs text-slate-400'>Deployed packages</span>
           </div>
           <Separator orientation='vertical' className='h-[100px]' />
-          <div className='w-[200px] flex flex-col items-center justify-center'>
-            <span className='text-2xl font-bold flex flex-row items-center gap-2'><FileSearch2 strokeWidth={1.25} className='h-5 w-5'/>700+</span>
-            <span className='text-slate-400'>Objects views</span>
+          <div className='w-full md:w-[200px] flex flex-col items-center justify-center'>
+            <span className='text-xs font-bold flex flex-row items-center gap-2'><FileSearch2 strokeWidth={1.25} className='h-5 w-5' />700+</span>
+            <span className='text-xs text-slate-400'>Objects views</span>
           </div>
           <Separator orientation='vertical' className='h-[100px]' />
-          <div className='w-[200px] flex flex-col items-center justify-center'>
-            <span className='text-2xl font-bold flex flex-row items-center gap-2'><Users strokeWidth={1.25} className='h-5 w-5'/>500+</span>
-            <span className='text-slate-400'>Developers</span>
+          <div className='w-full md:w-[200px] flex flex-col items-center justify-center'>
+            <span className='text-xs font-bold flex flex-row items-center gap-2'><Users strokeWidth={1.25} className='h-5 w-5' />500+</span>
+            <span className='text-xs text-slate-400'>Developers</span>
           </div>
         </div>
       </div>
       {/* <Separator /> */}
-      <div className='w-full mt-[100px] min-h-[500px] flex flex-col items-center justify-start '>
-        <span className='mt-10 text-4xl font-bold text-slate-100'><span className='bg-gradient-to-r from-purple-300 to-indigo-500 text-transparent bg-clip-text'>Loved</span> by Developers</span>
-        <div className='w-fit grid grid-cols-3 mt-10 gap-2'>
-          <div className='border rounded-xl w-[300px] h-[100px] shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-2 text-slate-300'>
-            {'\"I am using movestudio and feel it is very effective in my learning of move language...\"'}
+      <div className='w-full mt-[100px] min-h-[500px] flex flex-col items-center justify-start'>
+        <span className='mt-10 text-4xl font-bold text-slate-100 text-center'><span className='bg-gradient-to-r from-purple-300 to-indigo-500 text-transparent bg-clip-text'>Loved</span> by Developers</span>
+        <div className='w-full md:w-fit grid grid-cols-1 md:grid-cols-3 mt-10 gap-4 px-4'>
+          <div className='border rounded-xl w-full md:w-[300px] h-auto shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-4 text-slate-300'>
+            {'"I am using movestudio and feel it is very effective in my learning of move language..."'}
           </div>
-          <div className='border rounded-xl w-[300px] h-[100px] shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-2 text-slate-300'>
-            {'\"Honestly not sure how I\'d deploy modules to mainnet without it\"'}
+          <div className='border rounded-xl w-full md:w-[300px] h-auto shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-4 text-slate-300'>
+            {'"Honestly not sure how I\'d deploy modules to mainnet without it"'}
           </div>
-          <div className='border rounded-xl w-[300px] h-[100px] shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-2 text-slate-300'>
-            {'\"I do consider your IDE a brilliant and super useful achievement for Sui blockchain development\"'}
+          <div className='border rounded-xl w-full md:w-[300px] h-auto shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-4 text-slate-300'>
+            {'"I do consider your IDE a brilliant and super useful achievement for Sui blockchain development"'}
           </div>
-          <div className='border rounded-xl w-[300px] h-[100px] shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-2 text-slate-300'>
-            {'\"web-based IDE is much more user-friendly\"'}
+          <div className='border rounded-xl w-full md:w-[300px] h-auto shadow-[0_0_40px_-10px_rgb(0,0,0,0.3)] shadow-indigo-500/50 p-4 text-slate-300'>
+            {'"web-based IDE is much more user-friendly"'}
           </div>
         </div>
       </div>
