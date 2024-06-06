3de8d1cc92ffe4acfda28747ba05be43496d0d6e
Daniel Leavitt <daniel.7039@hotmail.com>
2024-06-06 14:45:12 -0700

    backend
    Add check for existing directories

--- move-studio-backend/src/build.ts
+++ move-studio-backend/src/build.ts
@@ -25,6 +25,9 @@ type TestResult = {
 function createProjectInFileSystem(files: IFile[], path = '') {
 
   // Create root directory
+  if (fs.existsSync(path)) {
+    fs.rmdirSync(path);
+  }
   fs.mkdirSync(path);
 
   // Create files and folder
